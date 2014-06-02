54dfaca9ed41de05c234ce56321bbd3dbcc2d817
straup <copea@si.edu>
2014-06-02 15:24:24 -0400

    start sanitizing paths

--- flask/server.py
+++ flask/server.py
@@ -6,6 +6,7 @@ from flask_cors import cross_origin
 import StringIO
 import dither
 
+import os.path
 import logging
 logging.basicConfig(level=logging.DEBUG)
 
@@ -21,16 +22,29 @@ def ping():
 @cross_origin(methods=['GET'])
 def atk():
 
-    # root = app.config.from_envvar('PLUMBING_ATKINSON_SERVER_IMAGE_ROOT')
-
-    d = dither.dither()
-
     src = flask.request.args.get('path')
+
+    src = src.replace("../", "")
+    src = os.path.relpath(src)
+
+    # This is not working yet...
+
+    try:
+        root = app.config.from_envvar('PLUMBING_ATKINSON_SERVER_IMAGE_ROOT')
+        src = os.path.join(root, src)
+    except Exception, e:
+        logging.info("no envvar")
+        pass
+
+    if not os.path.exists(src):
+        flask.abort(404)
+
     dest = StringIO.StringIO()
 
+    d = dither.dither()
     d.dither_image(src, dest, 'GIF')
-    dest.seek(0)
 
+    dest.seek(0)
     return flask.send_file(dest, mimetype='image/gif')
 
 if __name__ == '__main__':
