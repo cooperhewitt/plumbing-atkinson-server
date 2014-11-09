55bc47697e2b4352bdad41daee9f5eb78bd9be93
straup <github@aaronland.net>
2014-11-09 11:54:56 -0500

    start making it work with http_pony

--- flask/server.py
+++ flask/server.py
@@ -2,64 +2,64 @@
 
 import flask
 from flask_cors import cross_origin 
-from werkzeug.security import safe_join
 
 import StringIO
-import dither
-
-import os
-import os.path
 import logging
-logging.basicConfig(level=logging.INFO)
 
-app = flask.Flask(__name__)
-app.config['USE_X_SENDFILE'] = True
+try:
+    # https://github.com/cooperhewitt/py-cooperhewitt-roboteyes-atkinson
+    import cooperhewitt.roboteyes.atkinson as atkinson
+except Exception, e:
+    import atkinson
 
-# Quick. And dirty. To figure out:
-# http://flask.pocoo.org/docs/config/
-# https://github.com/mbr/flask-appconfig
-# (20140602/straup)
-
-if os.environ.get('ATKINSON_SERVER_IMAGE_ROOT', None):
-    app.config['ATKINSON_SERVER_IMAGE_ROOT'] = os.environ['ATKINSON_SERVER_IMAGE_ROOT']
+try:
+    # https://github.com/cooperhewitt/py-cooperhewitt-flask
+    import cooperhewitt.flask.http_pony as http_pony
+except Exception, e:
+    import http_pony
 
 @app.route('/ping', methods=['GET'])
 @cross_origin(methods=['GET'])
 def ping():
-    rsp = {'stat': 'ok'}
-    return flask.jsonify(**rsp)
 
-@app.route('/atk', methods=['GET'])
-def atk():
+    return flask.jsonify({'stat': 'ok'})
 
-    src = flask.request.args.get('path')
-    logging.debug("request path is %s" % src)
-
-    root = app.config.get('ATKINSON_SERVER_IMAGE_ROOT', None)
-
-    if root:
-        safe = safe_join(root, src)
-
-        if not safe:
-            logging.error("'%s' + '%s' considered harmful" % (root, src))
-            flask.abort(400)
-
-        src = safe
-
-    logging.debug("final request path is %s" % src)
-    
-    if not os.path.exists(src):
-        logging.error("%s does not exist" % src)
-        flask.abort(404)
+@app.route('/dither', methods=['GET'])
+def dither():
 
     dest = StringIO.StringIO()
 
-    d = dither.dither()
-    d.dither_image(src, dest, 'GIF')
+    try:
+        atkinson.dither()
+    except Exception, e:
+        logging.error("failed to dither")
+        flask.abort(500)
 
     dest.seek(0)
     return flask.send_file(dest, mimetype='image/gif')
 
 if __name__ == '__main__':
-    debug = True	# sudo make me a CLI option
-    app.run(debug=debug)
+
+    import sys
+    import optparse
+    import ConfigParser
+
+    parser = optparse.OptionParser()
+
+    parser.add_option("-c", "--config", dest="config", help="", action="store", default=None)
+    parser.add_option("-v", "--verbose", dest="verbose", help="enable chatty logging; default is false", action="store_true", default=False)
+
+    opts, args = parser.parse_args()
+
+    if opts.verbose:
+        logging.basicConfig(level=logging.DEBUG)
+        logging.debug("verbose logging is enabled")
+    else:
+        logging.basicConfig(level=logging.INFO)
+
+    cfg = ConfigParser.ConfigParser()
+    cfg.read(opts.config)
+
+    http_pony.update_app_config(app, cfg)
+
+    app.run()
